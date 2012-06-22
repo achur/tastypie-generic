6a7eaf3b60cb29fe0e738b9e343b6ac99fed1303
Alex Churchill <alex@premonit.com>
2012-06-22 12:56:03 -0700

    Package for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tastypie-generic',
+      version='0.0.1',
+      description='Generics Toolkit for Django-Tastypie',
+      author='Alex Churchill',
+      author_email='alex@premonit.com',
+      url='https://github.com/achur/tastypie-generic',
+      packages=['tastypie_generic'],
+     )
